6c16c31dc44b346e5e08f8165de30002d3e6edd2
AmandaOh <amandaoh@thoughtworks.com>
2018-07-11 22:45:18 +0800

    Extract TodoList to its own component

--- app/src/App.js
+++ app/src/App.js
@@ -1,71 +1,10 @@
 import React, {Component} from 'react';
-import {List, Map} from 'immutable';
-import Icon from '@material-ui/core/Icon';
 
 import logo from './logo.svg';
 import './App.css';
+import TodoList from './TodoList';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: List([])
-    };
-  }
-
-  async componentDidMount() {
-    const todos = await this.getTodos();
-    this.setState({data: List(todos)});
-  }
-
-  async getTodos() {
-    try {
-      const response = await fetch('/todos');
-      return response.json();
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  handleChange(index, e) {
-    const currentTodos = this.state.data;
-    const updatedTodos = currentTodos.update(index, item => Map(item).set("todo", e.target.value).toObject());
-
-    this.setState({data: updatedTodos});
-  }
-
-  handleKeyDown(id, e) {
-    if (e.key === 'Enter') {
-      this.updateTodoList(id, e.target.value);
-    }
-  }
-
-  async updateTodoList(id, updatedTodo) {
-    try {
-      const response = await fetch(`/todos/${id}`, {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({id: id, todo: updatedTodo})
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  addAnotherRow(index, e) {
-    const currentTodos = this.state.data;
-    const lastId = currentTodos.get(-1).id;
-
-    const updatedTodos = currentTodos.insert(index + 1, {
-      id: lastId + 1,
-      todo: ""
-    })
-
-    this.setState({data: updatedTodos})
-  }
 
   render() {
     return (<div className="App">
@@ -73,17 +12,7 @@ class App extends Component {
         <img src={logo} className="App-logo" alt="logo"/>
         <h1 className="App-title">Todo List with React</h1>
       </header>
-      {
-        this.state.data && <ul>
-            {
-              this.state.data.map((item, index) =>
-              <li>
-                <input key={item._id} type="text" value={item.todo} onChange={(e) => this.handleChange(index, e)} onKeyDown={(e) => this.handleKeyDown(item.id, e)} className="todo-input"/>
-                <Icon color="primary" onClick={(e) => this.addAnotherRow(index, e)} className="add-todo">add_circle</Icon>
-              </li>)
-            }
-          </ul>
-      }
+      <TodoList />
     </div>);
   }
 }
